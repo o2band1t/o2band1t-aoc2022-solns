39f1f2634fcb1f3cb4034814c2aff4afaf50d9e6
Matthew <matthewlucos@gmail.com>
2022-12-13 06:50:58 +0800

    finished day 4

---
+++ day4p1.py
@@ -0,0 +1,13 @@
+with open('day4_input.txt', 'r') as f:
+	raw = [l.replace('\n', '').split(',') for l in f.readlines()]
+
+pairs = 0
+
+for l in raw:
+	r0 = tuple(map(int, l[0].split('-')))
+	r1 = tuple(map(int, l[1].split('-')))
+	if (r0[0] <= r1[0]) and (r0[1] >= r1[1]) \
+	or (r1[0] <= r0[0]) and (r1[1] >= r0[1]):
+		pairs += 1
+
+print(pairs)
