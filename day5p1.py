bb4d74c3076a23fa0b1d20e99f27635f6fbc45e7
Matthew <matthewlucos@gmail.com>
2022-12-13 07:24:21 +0800

    finished day 5

---
+++ day5p1.py
@@ -0,0 +1,28 @@
+with open('day5_input.txt', 'r') as f:
+	raw = [l.replace('\n', '') for l in f.readlines()]
+
+stacks = {
+	1: list('LNWTD'),
+	2: list('CPH'),
+	3: list('WPHNDGMJ'),
+	4: list('CWSNTQL'),
+	5: list('PHCN'),
+	6: list('THNDMWQB'),
+	7: list('MBRJGSL'),
+	8: list('ZNWGVBRT'),
+	9: list('WGDNPL')
+}
+
+message = ''
+
+for l in raw:
+	for w in ('move', 'from', 'to'):
+		l = l.replace(w, '')
+	move, from_, to = [int(n) for n in l.split(' ') if n != '']
+	for _ in range(int(move)):
+		stacks[to].append(stacks[from_].pop())
+
+for s in stacks.values():
+	message += s.pop()
+
+print(message)
